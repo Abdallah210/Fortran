a67d328aed3862b75975ae1baff3fe71b4577a8d
Elfilali210 <abdallah.elfilali.etu@univ-lille.fr>
2025-09-28 15:23:57 +0200

    TP2

---
+++ TP2/vitesse.f90
@@ -0,0 +1,21 @@
+program vitesse
+
+  g = 9.81
+  m = 81.
+  alpha = 12.5
+  v0 = 0.
+  t = 0.
+  tf = 200.
+  dt = 0.1
+200 v1 = v0 + dt * (g - (alpha/m) * v0)
+  t = t + dt
+  if (t.lt.tf) then
+     v0 = v1
+     write(10,*)t,v1
+     write(*,*)t,v1
+     goto 200
+  endif
+
+  stop
+end program vitesse
+
